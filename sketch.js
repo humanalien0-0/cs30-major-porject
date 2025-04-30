7109f965eebdedcf338bdae5ba7b1477aa8a876a
Jayawickrama7009611 <7009611@spsd.sk.ca>
2025-04-30 14:16:11 -0600

    changed the game idea, making bunch of mini games
    researching about mini games

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@
 // log
 // April 17th - started researchng about p5.party to create multiplayer function of the game 
 // APRIL 29TH - RESEARCHING ON CHESS BOTS 
+// April 30th - Researchig about new mini games, planning to change in proposal.md
+//
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
