a6cd551b4a46cc3cbc9308f54562d48b0b7abf2b
humanalien0-0 <152632971+humanalien0-0@users.noreply.github.com>
2025-05-06 14:12:05 -0600

    searching for textures

--- sketch.js
+++ sketch.js
@@ -5,13 +5,6 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// log
-// April 17th - started researchng about p5.party to create multiplayer function of the game 
-// APRIL 29TH - RESEARCHING ON CHESS BOTS 
-// April 30th - Researchig about new mini games, planning to change in proposal.md
-// May 1st - updating proposals.md 
-// May 2nd - building up basic logic/procedure for Happy connect
-
 // Happy connect procedure
 // step 1 - create a square grid with pairs of dots with the same color placed in it
 // step 2 - create a function to control the path of the dots with the mouse
